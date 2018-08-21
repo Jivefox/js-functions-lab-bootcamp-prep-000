986deab5d941833c4235805764df3689199356e7
Cliff Jurgensen <jivefox.002@gmail.com>
2018-08-21 17:22:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 function happyHolidays() {
   return ('Happy Holidays!')
 }
+function happyHolidaysTo(name) {
+  return (`Happy Holidays, ${name}!`)
+}
+function happyHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}!`)
+}
